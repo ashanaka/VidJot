7898e09ccb116666768fa2f9367b0c5feb05ebda
Ashan Shanaka <44833876+ashanaka@users.noreply.github.com>
2019-10-22 06:05:06 +0530

    Uploaded files
    Created the repository in Github and uploaded the project files

---
+++ app.js
@@ -0,0 +1,119 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const port = 5000;
+
+const app = express();
+
+//Map global promis - get rid of Warning
+mongoose.Promise = global.Promise;
+
+//connect to the mongoose
+mongoose.connect('mongodb://localhost/vidjot-dev', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+    // useMongoClient: true
+}).then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+
+//Idea Index Page
+app.get('/ideas', (req, res) => {
+    Idea.find({})
+        .sort({ date: 'desc' })
+        .then(ideas => {
+            res.render('ideas/index', {
+                ideas: ideas
+            })
+        });
+});
+
+//Load model
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
+
+//How middleware works
+app.use(function (req, res, next) {
+    req.name = 'Ashan Shanaka';
+    next();
+});
+
+//Handlebars Middleware
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+//Body parser middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+//Index Route
+app.get('/', (req, res) => {
+    const title = "Welcome";
+    res.render('index', {
+        title: title
+    });
+});
+
+//About Route
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+//Add Ideas Forum
+app.get('/ideas/add', (req, res) => {
+    res.render('ideas/add');
+});
+
+//Edit Ideas
+app.get('/ideas/edit/:id', (req, res) => {
+    Idea.findOne({
+        _id: req.params.id
+    })
+        .then(idea => {
+            res.render('ideas/edit', {
+                idea: idea
+            });
+        });
+});
+
+//Process Form
+app.post('/ideas', (req, res) => {
+    let errors = [];
+    if (!req.body.title) {
+        errors.push({
+            text: 'Please Add The Title'
+        });
+    }
+    if (!req.body.details) {
+        errors.push({
+            text: 'Please Add Some Details'
+        })
+    }
+
+    if (errors.length > 0) {
+        res.render('ideas/add', {
+            errors: errors,
+            title: req.body.title,
+            details: req.body.details
+        });
+    } else {
+        const newUser = {
+            title: req.body.title,
+            details: req.body.details
+        }
+        new Idea(newUser)
+            .save()
+            .then(idea => {
+                res.redirect('/ideas');
+            })
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server started port ${port}`);
+});
