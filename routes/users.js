c37781d65925fb250ea5a829a1b04558e4456a91
Ashan Shanaka <jpashanshanaka@gmail.com>
2020-04-06 13:14:02 +0530

    bug fixed for local

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const passport = require('passport');
 const router = express.Router();
 
